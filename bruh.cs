81c7352e94bcf6c481666d8cabdcfac544063386
Tiko <noah@thebauers.net>
2021-08-13 15:19:00 -0600

    how is the documentation so god awful i don't understand
    I've followed several different tutorials and not even the god damn docs made BY THE PEOPLE WHOS API IM TRYING TO USE DOESNT EVEN WALK ME THROUGH EVERY STEP
    ITS LIKE ID HAVE TO HAVE DEVELOPED AT LEAST 15 DIFFERENT APPLICATIONS BEFORE THIS TO EVEN BEGIN TO FOLLOW THE DIRECTIONS
    FOR GODS SAKE THE CODE FOR THE COMPLETED STARTER BOT THAT THEY LINK TO BARELY RESEMBLES THE FUCKING STESPS THEY WALKED ME THROUGH GOD

---
+++ bruh.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+{
+	static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	async Task MainAsync()
+	{
+	}
+}
